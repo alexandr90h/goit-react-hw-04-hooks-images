3c7b12d35416ddecade7ef0a647b3b813b8bede7
Alexandr <63474923+alexandr90h@users.noreply.github.com>
2020-12-22 00:22:06 +0200

    22.12.2020-1

--- src/App.js
+++ src/App.js
@@ -1,77 +1,82 @@
 import './App.scss';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
-import React, { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar.jsx';
 import ImageGallery from './ImageGallery/ImageGallery.jsx';
 import Button from './Button/Button';
 import Modal from './Modal/Modal';
+import { useEffect, useState } from 'react';
 
-class App extends Component{
-  state = {
-    keyWord: '',
-    page: 1,
-    key: '18956584-3ac01e2418e4c39c7eb5dacd9',
-    images: [],
-    loader: false,
-    showModal: false,
-    buttonVisible: false,
-    imgIdModal: '',
-    newPageCords: 0,
-  }
-  toggleLoader = () => {
-    this.setState(prev => ({ loader: !prev.loader }));
-  }
-  toggleModal = () => {
-    this.setState(prev => ({ showModal: !prev.showModal }));
-  }
-  onSubHandApp = data => {
-    this.setState({ keyWord: data, page:1, images:[] })
-  }
-  onSubPageNumApp = data => {
-        const cords = document.documentElement.scrollHeight - 170;
-    this.setState({ page: data,newPageCords: cords })
-  }
-  onSubIdHendApp = data => {
-    this.toggleLoader();
-        this.toggleModal();
-    this.setState({ imgIdModal: data });
-        this.toggleLoader();
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.keyWord !== this.state.keyWord || prevState.page !== this.state.page) {
-      this.toggleLoader();
-      fetch(`https://pixabay.com/api/?q=${this.state.keyWord}&page=${this.state.page}&key=${this.state.key}&image_type=photo&orientation=horizontal&per_page=12`)
-        .then(res => res.json()).then(({ hits }) => { this.setState(prev => ({ images: prev.images.concat(hits) }));if (this.state.images.length!==0){this.setState({buttonVisible:true})}
-        }).finally(() => this.toggleLoader());
+export default function App() {
+    const [keyWord, setKeyWord] = useState(null);
+    const [page, setPage] = useState(1);
+    const key='18956584-3ac01e2418e4c39c7eb5dacd9';
+    const [images, setImages] = useState([]);
+    const [imgIdModal, setImgIdModal] = useState('');
+    const [newPageCords, setNewPageCords] = useState(0);
+    const [loader, setLoader] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [buttonVisible, setButtonVisible] = useState(false);
+
+    const toggleLoader = () => {
+        setLoader(prev => !prev);
     }
-        this.scrollToNextPage();
-  }
-  scrollToNextPage = () => {
-    const { newPageCords } = this.state;
-    window.scrollTo({
-      top: newPageCords,
-      behavior: 'smooth',
-    });
-  };
-  render() {
+    const toggleModal = () => {
+        setShowModal(prev => !prev);
+    }
+    const onSubKeyWordHandApp = data => {
+        setKeyWord(data);
+        setPage(1);
+        setImages([]);
+        console.log('onSubKeyWordHandApp')
+    }
+    const onSubPageNumApp = data => {
+        const cords = document.documentElement.scrollHeight - 170;
+        setPage(data);
+        setNewPageCords(cords);
+    }
+    const onSubIdHendApp = data => {
+        toggleLoader();
+        toggleModal();
+        setImgIdModal(data);
+        toggleLoader();
+    }
+    const scrollToNextPage = () => {
+        window.scrollTo({
+            top: newPageCords,
+            behavior: 'smooth',
+        });
+    }
+
+    useEffect(() => {
+        if (keyWord) {
+            toggleLoader();
+            fetch(`https://pixabay.com/api/?q=${keyWord}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`)
+                .then(res => res.json()).then(({ hits }) => {
+                    setImages(prev => prev.concat(hits));
+                    if (images.length !== 0) { setButtonVisible(prev => prev = true) }
+                }).finally(toggleLoader());
+            scrollToNextPage();
+        }
+        console.log(keyWord);
+        console.log(images);
+        console.log(buttonVisible);
+    }, [keyWord, page]);
+    
     return (<div className="main-conteiner">
-      <Searchbar onSubHand={this.onSubHandApp} />
-      <ImageGallery gallery={this.state.images} onSubIdHends={ this.onSubIdHendApp}/>
-      {this.state.loader && <Loader
+      <Searchbar onSubHand={onSubKeyWordHandApp} />
+      <ImageGallery gallery={images} onSubIdHends={ onSubIdHendApp}/>
+      {loader && <Loader
         type="ThreeDots"
         color="#00BFFF"
         height={100}
         width={100}
       />}
-      {this.state.buttonVisible && <Button onSubPageNum={this.onSubPageNumApp} />}
-      {this.state.showModal &&
+      {buttonVisible && <Button onSubPageNum={onSubPageNumApp} />}
+      {showModal &&
         <Modal
-        onCloseHend={this.toggleModal}
-        imgId={this.state.imgIdModal}/>}
+        onCloseHend={toggleModal}
+        imgId={imgIdModal}/>}
     </div>
     );
 }
-}
-
-export default App;
